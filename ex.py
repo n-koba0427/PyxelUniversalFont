725fcf1ea8e10c321f2bd83ffabfaa34bd0484d8
Naoki Kobayashi <n.koba0427@gmail.com>
2023-09-25 18:02:53 +0900

    readme image

--- ex.py
+++ ex.py
@@ -10,6 +10,6 @@ pyxel.cls(7)
 
 # draw(x座標, y座標, テキスト, フォントサイズ, 文字の色)
 # 背景色はデフォルト値(-1:透明)
-writer.draw(25, 4, "PyselUniversalFont", 50, 16)
+writer.draw(25, 4, "PyxelUniversalFont", 50, 16)
 
 pyxel.show()
